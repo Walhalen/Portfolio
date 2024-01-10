f7dd068383c2ba870f87af0c22f46377ed169bf2
exit <exit>
2024-01-10 10:10:26 +0200

    ForME

--- portfolio/src/Components/ForMeInfo.tsx
+++ portfolio/src/Components/ForMeInfo.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import az from '../assets/Az.jpg';
+
 
 export const ForMeInfo = () => {
   return (
     <div className='forMeInfo'>
         <div>
-            <h3>
-                Hi I am Ivan,
+            <h3 style={{width: "500px", height: "fit-content"}}>
+              Hello my name is Ivan Postolov.
             </h3>
-            <h4 className='infoText2'> 
-                Now I am 12 grate and already have some interesting projects 
-                and know bunch of programming Langueges.
-            </h4>
+            <h1 className='infoText2'> 
+            I study at TUES and I am 12th grade. Programming is a big part of my life. You can see that by looking at this portfolio. I have studied a lot of languages presented below. I also have a few small and big projects that you might be interested in looking at.    
+            </h1>
         </div>
 
         <div className='myImages'>
-            
+            <img src={az} alt="Who am I" />
         </div>
     </div>
   )
--- portfolio/src/pages/HomePage.tsx
+++ portfolio/src/pages/HomePage.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Header } from '../Components/Header'
 import { ForMeInfo } from '../Components/ForMeInfo'
+import { FaArrowCircleDown } from "react-icons/fa";
 
 export const HomePage = () => {
   return (
     <div>
-        <div style = {{display: "flex"}}>
+        <div style = {{display: "flex", justifyContent: 'center', gap: "100px"}}>
           <ForMeInfo/>
-          <Header/>
+          <div >
+            <Header/>
+            <div style={{marginTop: "50px", display: 'flex', flexDirection: "column", alignItems: "center"}}>
+               <h1 className='secondInfoText'>
+                  I have a lot more interests. I can play the guitar, I like to go to the mountains, I like to run. However, if you want to see my interest in programming go down.
+               </h1>
+               <FaArrowCircleDown className="arrowIconDown"/>
+            </div>
+          </div>
+          
         </div>
         
         <main>
